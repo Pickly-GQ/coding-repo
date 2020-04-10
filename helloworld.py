a87a94b7af4630fa47bceb9997de85904321a92c
Pickly-GQ <62808403+Pickly-GQ@users.noreply.github.com>
2020-04-10 12:23:12 -0500

    forgot to add some +s
    oh wut. im tie.d0iaeojigen er

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
 print("hello world.")
 ask = input("you are good or bad day?")
-print("cool, you are having a " + ask "day.")
-print("bye and enjoy your " + ask "day!")
+print("cool, you are having a " + ask + "day.")
+print("bye and enjoy your " + ask + "day!")
